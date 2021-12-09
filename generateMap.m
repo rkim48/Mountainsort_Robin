00421c01f60daf202526412320aeb7c3ef817cd9
Robin <rk48@rice.edu>
2021-12-08 22:12:46 -0600

    generate changed map based on recorded channels and other stuff

--- generateMap.m
+++ generateMap.m
@@ -1,20 +1,23 @@
 % Intan channel 38 is the shallowest electrode while 25 is the deepest. 
 % Let channel 38 be at position (0,0). 
-Intan_ch = [25 47 27 45 29 43 31 41 23 33 21 35 19 37 17 39 24 46 26 44 28 42 30 40 22 32 20 34 18 36 16 38];
-nCH = numel(Intan_ch);
-pitch = 60; % um
-x = zeros(nCH,1);
-y_presort =zeros(nCH,1);
-for i = 1:nCH
-    y_presort(i) =pitch * (i-1); 
+function map = generateMap(intan_ch,good_ch,save_dir)
+    Intan_ch = [25 47 27 45 29 43 31 41 23 33 21 35 19 37 17 39 24 46 26 44 28 42 30 40 22 32 20 34 18 36 16 38];
+    nCH = numel(Intan_ch);
+    pitch = 60; % um
+    x = zeros(nCH,1);
+    y_presort =zeros(nCH,1);
+    for i = 1:nCH
+        y_presort(i) =pitch * (i-1); 
+    end
+    pos_arr = [(1:nCH)' flip(Intan_ch') y_presort]; % indices, Intan channels, y_coordinates
+
+    [sorted_Intan_ch,sorted_idx] = sort(flip(Intan_ch));
+    y = y_presort(sorted_idx);
+
+    ordered_pos_arr =  [(1:nCH)' sorted_Intan_ch' y];  % pos_arr but ordered with respect to Intan channel order
+
+    map = [x y];
+    map = map(good_ch,:);
+
+    writematrix(map,fullfile(save_dir,'geom.csv'))
 end
-pos_arr = [(1:nCH)' flip(Intan_ch') y_presort]; % indices, Intan channels, y_coordinates
-
-[sorted_Intan_ch,sorted_idx] = sort(flip(Intan_ch));
-y = y_presort(sorted_idx);
-
-ordered_pos_arr =  [(1:nCH)' sorted_Intan_ch' y];  % pos_arr but ordered with respect to Intan channel order
-
-map = [x y];
-
-writematrix(map,'geom.csv')
---
+++ parsaveDataSeg.m
@@ -0,0 +1,3 @@
+function parsaveDataSeg(fname, data_seg)
+save(fname, 'data_seg','-v6')
+end
