ff4826771ae3862c3b7450fd141a6294a9c6c968
Robin <rk48@rice.edu>
2021-12-13 11:50:10 -0600

    removed/moved files

--- load_Intan_data_wrapper.m
+++
@@ -1,6 +0,0 @@
-function [amplifier_data,amplifier_channels,frequency_parameters,ADC] = load_Intan_data_wrapper(filename,selection)
-
- 
-[amplifier_data,amplifier_channels,frequency_parameters,ADC]=read_Intan_RHD2000_fileV3(filename,selection);
-
-end
